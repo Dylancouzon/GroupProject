7823004fc33b4112d7bcd96d0f79511af1e41706
Dylancouzon <dylancouzon@gmail.com>
2021-04-05 14:53:44 -0700

    Return result of getVaccineData && getCovidData in arrays

--- assets/api/api.js
+++ assets/api/api.js
@@ -1,15 +1,15 @@
-var CovidData;
-var stateName = "Alabama";
-//Runs all the APis with the stateID
-function runAPIs(stateID) {
-
-    getCovidData(stateID);
+var returnCovidData;
+var returnVaccineData;
+//Runs all the APis with the stateName
+function runAPIs(stateName) {
+    //Need to add a function that checks if the Name is valid
+    getCovidData(stateName);
     getVaccineData(stateName);
 }
 
 // Api call to get the covid Data using the XMLHttpRequest Object
 //Return the result with renderCovidData 
-function getCovidData(stateID) {
+function getCovidData(stateName) {
     const data = null;
 
     const xhr = new XMLHttpRequest();
@@ -18,19 +18,16 @@ function getCovidData(stateID) {
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             var covidData = JSON.parse(this.response);
-            //Returns the confirmed, recovered and deaths in the last 24hrs
-            // Needs to be activated
-            // renderCovidData(confirmed, recovered, deaths);
-            // renderCovidData(covidData[0].provinces[stateID].confirmed, covidData[0].provinces[stateID].recovered, covidData[0].provinces[stateID].deaths);
-            // OR
-            // CovidData = [confirmed, recovered, deaths];
-            CovidData = [covidData[0].provinces[stateID].confirmed, covidData[0].provinces[stateID].recovered, covidData[0].provinces[stateID].deaths];
-            // OR
-            //renderCovidData = {
-            //confirmed: xx,
-            // recovered: xx,
-            //deaths: xx
-            //}
+
+            //Returns the confirmed, recovered and deaths in the last 24hrs           
+            for(i=0; i < covidData[0].provinces.length; i++){
+                if (covidData[0].provinces[i].province == stateName) {
+                     // returnCovidData = [confirmed, recovered, deaths];
+                    returnCovidData = [covidData[0].provinces[i].confirmed, covidData[0].provinces[i].recovered, covidData[0].provinces[i].deaths];
+                    //Break the loop once we have the value needed
+                    break;
+                }
+            }
         }
     });
 
@@ -49,23 +46,31 @@ function getVaccineData(stateName) {
     Papa.parse(url, {
         download: true,
         complete: function (results) {
-            //need to look inside the array where date == date and stateName == statename
-            console.log(results.data);
+            //need to look inside the array where results.data.date == date and results.data.stateName == statename
+            for(i=0; i < results.data.length; i++){
+                if (results.data[i][0] == date && results.data[i][1] == stateName) {
+                    // returnVaccineData = Total number of shots given, number of people vaccinated, percentage of people vaccinated, number of people fully vaccinated, daily vaccinations
+                    returnVaccineData = [results.data[i][2], results.data[i][4], results.data[i][5], results.data[i][7], results.data[i][11]];
+                    console.log(returnVaccineData);
+                    //Break the loop once we have the value needed
+                   break;
+               }
+            }
         }
     });
 }
 
 //Get today's date
 // Function base on https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
-function getDate(){
+function getDate() {
     var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
-var yyyy = today.getFullYear();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
 
-today = yyyy + "-" + mm + "-" + dd;
-return today;
+    today = yyyy + "-" + mm + "-" + dd;
+    return today;
 }
 
 //Temporarily run the function
-runAPIs(1);
+runAPIs("California");
